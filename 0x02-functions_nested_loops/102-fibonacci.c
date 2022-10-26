9604b2847faf88c321220ca5a6dbe7e5c03bb0b7
Paul1o1 <pablonigussie1@gmail.com>
2022-10-26 10:41:38 -0700

    advanced 3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,11 +15,11 @@ int main(void)
 	for (c = 0; c < 48; c++)
 {
 	fn = n1 + n2;
-	printf(", %ld, fn);
+	printf(", %ld", fn);
 	n1 = n2;
-	n2 =fn;
+	n2 = fn;
 }
-printf("\n");
+printf("/n");
 return (0);
 }
 
